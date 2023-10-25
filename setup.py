7a3586b6f6dc34238a6abc59fcfd9a9ce4a64bc4
Alexandr Lukoyanov <alexandr@vainu.io>
2023-10-25 21:36:24 +0300

    Make it work with python3

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from distutils.core import setup, Extension
 
-langid = Extension("_langid", 
-                   language = 'c',
-                   libraries = ['protobuf-c'],
-                   sources = ["_langid.c", "liblangid.c", "model.c", "sparseset.c", "langid.pb-c.c"],
-                   )
+langid = Extension(
+    "_langid",
+    language='c',
+    libraries=['protobuf-c'],
+    sources=["_langid.c", "liblangid.c", "model.c", "sparseset.c", "langid.pb-c.c"],
+)
 
 setup(
+    name='langid',
+    version='1.0.0',
     ext_modules=[langid],
 )
